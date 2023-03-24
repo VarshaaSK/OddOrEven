1df81f4283e1a6a8468d805cb73a424b44f7fd6a
VarshaaSK <varshaa.sk@m2pfintech.com>
2023-03-24 23:52:18 +0530

    Add. Included additional prompt Statement

--- src/OddEven.java
+++ src/OddEven.java
@@ -1,5 +1,6 @@
 public class OddEven {
     public static void main(String[] args) {
+        System.out.println("Odd or Even");
         int number = 5;
         if(number % 2 == 0){
             System.out.println("Number is Even");
