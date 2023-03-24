f312ed11a0901f2b02a8d7cc0bb051a2dedc8dba
VarshaaSK <varshaa.sk@m2pfintech.com>
2023-03-24 23:33:29 +0530

    Add. Included the Logic

--- src/OddEven.java
+++ src/OddEven.java
@@ -1,5 +1,11 @@
 public class OddEven {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number = 5;
+        if(number % 2 == 0){
+            System.out.println("Number is Even");
+        }
+        else{
+            System.out.println("Number is Odd");
+        }
     }
 }
